3ed26b8d454c46733ebcc835fcc966655f4b9a41
rlrh <rlrh1996@gmail.com>
2019-09-19 15:32:00 +0800

    Fix SignInCallback page

--- frontend/src/pages/SignInCallback.js
+++ frontend/src/pages/SignInCallback.js
@@ -1,22 +1,25 @@
+import React, { useEffect } from 'react';
 import { Redirect } from 'react-router';
 import { useFetch } from 'react-async';
-import React, { useEffect } from 'react';
-import queryString from 'query-string';
-
 import { useAuth } from '../state/useAuth';
 
 const SignInCallback = ({ location }) => {
   const { signIn } = useAuth();
   const headers = { Accept: 'application/json' };
   const options = { credentials: 'include' };
-  const { data, error, isLoading, run } = useFetch(
+  const { data, error, isLoading } = useFetch(
     `http://127.0.0.1:8000/authorize/done/${location.search}`,
     { headers },
     options,
   );
-  if (data) {
-    console.log('Your Spotify access token is: ' + data.access_token);
-  }
+
+  useEffect(() => {
+    if (data) {
+      console.log('Your Spotify access token is: ' + data.access_token);
+      signIn(data.access_token);
+    }
+  }, [data]);
+
   return (
     <React.Fragment>
       {isLoading && <div> 'Loading...' </div>}
@@ -24,16 +27,5 @@ const SignInCallback = ({ location }) => {
       {data && <Redirect to="/" />}
     </React.Fragment>
   );
-  /*
-  useEffect(() => {
-    const values = queryString.parse(location.search);
-    const accessToken = values.access_token;
-    if (values) {
-    } else {
-      console.log('ERROR: could not parse Spotify access token');
-    }
-    signIn(accessToken);
-  }, []);
-*/
 };
 export default SignInCallback;
