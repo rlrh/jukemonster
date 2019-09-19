8124500a0fa753636068aab099c792dea28e93d0
rlrh <rlrh1996@gmail.com>
2019-09-19 21:19:13 +0800

    Fix useRoomState hook to use only 1 reducer

--- frontend/src/hooks/useRoomState.js
+++ frontend/src/hooks/useRoomState.js
@@ -2,6 +2,7 @@ import { useState, useReducer, useMemo } from 'react';
 import useWebSocket from 'react-use-websocket';
 
 const useRoomState = roomId => {
+  // Setup WebSocket connection hook
   const socketUrl = `ws://127.0.0.1:8000/ws/room/${roomId}/`;
   const STATIC_OPTIONS = useMemo(
     () => ({
@@ -17,32 +18,37 @@ const useRoomState = roomId => {
     STATIC_OPTIONS,
   );
 
-  // TODO: remove placeholder initial queue tracks
-  const initialQueuedTracks = [
-    {
+  // TODO: remove placeholder initial values
+  const initialValues = {
+    nowPlayingTrack: {
       id: '11dFghVXANMlKmJXsNCbNl',
-      name: 'Cut To The Feeling',
-      artists: ['Carly Rae Jepsen'],
-      album: 'Cut To The Feeling',
-      isExplicit: false,
-      imageSource:
-        'https://i.scdn.co/image/107819f5dc557d5d0a4b216781c6ec1b2f3c5ab2',
-      votes: 0,
     },
-    {
-      id: '7xGfFoTpQ2E7fRF5lN10tr',
-      name: 'Run Away With Me',
-      artists: ['Carly Rae Jepsen'],
-      album: 'Emotion (Deluxe)',
-      isExplicit: false,
-      imageSource:
-        'https://i.scdn.co/image/ff347680d9e62ccc144926377d4769b02a1024dc',
-      votes: 0,
-    },
-  ];
+    queuedTracks: [
+      {
+        id: '11dFghVXANMlKmJXsNCbNl',
+        name: 'Cut To The Feeling',
+        artists: ['Carly Rae Jepsen'],
+        album: 'Cut To The Feeling',
+        isExplicit: false,
+        imageSource:
+          'https://i.scdn.co/image/107819f5dc557d5d0a4b216781c6ec1b2f3c5ab2',
+        votes: 0,
+      },
+      {
+        id: '7xGfFoTpQ2E7fRF5lN10tr',
+        name: 'Run Away With Me',
+        artists: ['Carly Rae Jepsen'],
+        album: 'Emotion (Deluxe)',
+        isExplicit: false,
+        imageSource:
+          'https://i.scdn.co/image/ff347680d9e62ccc144926377d4769b02a1024dc',
+        votes: 0,
+      },
+    ],
+  };
 
   // TODO: return new state based on action type and payload
-  const queuedTracksReducer = (state, action) => {
+  const reducer = (state, action) => {
     switch (action.type) {
       case 'joinevent':
         console.log(action.payload);
@@ -56,13 +62,7 @@ const useRoomState = roomId => {
   };
 
   // State management
-  const [queuedTracks, dispatch] = useReducer(
-    queuedTracksReducer,
-    initialQueuedTracks,
-  );
-  const [nowPlayingTrack, setNowPlayingTrack] = useState({
-    id: '11dFghVXANMlKmJXsNCbNl',
-  });
+  const [state, dispatch] = useReducer(reducer, initialValues);
 
   // Let reducer handle state update
   function handleMessage(message) {
@@ -94,8 +94,8 @@ const useRoomState = roomId => {
   };
 
   return {
-    nowPlayingTrack,
-    queuedTracks,
+    nowPlayingTrack: state.nowPlayingTrack,
+    queuedTracks: state.queuedTracks,
     addTrack,
     upvoteTrack,
     downvoteTrack,
