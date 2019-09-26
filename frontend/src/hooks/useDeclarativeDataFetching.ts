b2fb9d5547e4d9ed36c459216336f6e75befa2e0
rlrh <rlrh1996@gmail.com>
2019-09-26 21:57:52 +0800

    Add useDeclarativeDataFetching

---
+++ frontend/src/hooks/useDeclarativeDataFetching.ts
@@ -0,0 +1,60 @@
+import { useEffect, useReducer } from 'react';
+
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const useDeclarativeDataFetching = (axiosPromiseFn, ..._arguments) => {
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: null,
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_INIT' });
+      try {
+        const result = await axiosPromiseFn(..._arguments);
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_FAILURE' });
+        }
+      }
+    };
+    fetchData();
+    return () => {
+      didCancel = true;
+    };
+  }, _arguments);
+
+  return state;
+};
+
+export default useDeclarativeDataFetching;
