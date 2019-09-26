94651e29462d7402b972d56ed27ae3f46764e3ee
Julius S <jluuiss@gmail.com>
2019-09-26 23:21:20 +0800

    Fix token refresh

--- frontend/src/apis/base.ts
+++ frontend/src/apis/base.ts
@@ -8,6 +8,7 @@ interface ApiHook {
   postApi: (path: string, body: object) => Promise<AxiosResponse<any>>;
   putApi: (path: string, body: object) => Promise<AxiosResponse<any>>;
   patchApi: (path: string, body: object) => Promise<AxiosResponse<any>>;
+  deleteApi?: (path: string) => Promise<AxiosResponse<any>>;
 }
 
 /**
@@ -17,60 +18,74 @@ interface ApiHook {
  */
 export const useOurApi: () => ApiHook = () => {
   const { value, ensureTokenValidity } = useAuth();
-  const getApi = async (path: string) => {
-    if (typeof value === 'string' || value == null) return null;
-    const getHeader = () => ({ Authorization: 'Bearer ' + value.access_token });
-    let resp = await axios.get(BACKEND_URL + path, { headers: getHeader() });
-    if (resp.status >= 400) {
-      await ensureTokenValidity();
-      resp = await axios.get(BACKEND_URL + path, { headers: getHeader() });
+
+  async function withRetry(
+    toCall: () => Promise<AxiosResponse<any>>,
+  ): Promise<AxiosResponse<any>> {
+    try {
+      const resp = await toCall();
+      return resp;
+    } catch (error) {
+      try {
+        await ensureTokenValidity();
+        const secondAttempt = await toCall();
+        return secondAttempt;
+      } catch (otherError) {
+        console.log(otherError);
+        throw otherError;
+      }
     }
-    return resp;
+  }
+
+  const getHeader = () => {
+    if (typeof value === 'string' || value == null) return null;
+    return { Authorization: 'Bearer ' + value.access_token };
+  };
+
+  const getApi = async (path: string) => {
+    const headers = getHeader();
+    if (!headers) return null;
+    return withRetry(() =>
+      axios.get(BACKEND_URL + path, { headers: getHeader() }),
+    );
   };
 
   const postApi = async (path: string, body: object) => {
-    if (typeof value === 'string' || value == null) return;
-    const getHeader = () => ({ Authorization: 'Bearer ' + value.access_token });
-    let resp = await axios.post(BACKEND_URL + path, body, {
-      headers: getHeader(),
-    });
-    if (resp.status >= 400) {
-      await ensureTokenValidity();
-      resp = await axios.post(BACKEND_URL + path, body, {
+    const headers = getHeader();
+    if (!headers) return null;
+    return withRetry(() =>
+      axios.post(BACKEND_URL + path, body, {
         headers: getHeader(),
-      });
-    }
-    return resp;
+      }),
+    );
   };
 
   const patchApi = async (path: string, body: object) => {
-    if (typeof value === 'string' || value == null) return;
-    const getHeader = () => ({ Authorization: 'Bearer ' + value.access_token });
-    let resp = await axios.patch(BACKEND_URL + path, body, {
-      headers: getHeader(),
-    });
-    if (resp.status >= 400) {
-      await ensureTokenValidity();
-      resp = await axios.patch(BACKEND_URL + path, body, {
+    const headers = getHeader();
+    if (!headers) return null;
+    return withRetry(() =>
+      axios.patch(BACKEND_URL + path, body, {
         headers: getHeader(),
-      });
-    }
-    return resp;
+      }),
+    );
   };
 
   const putApi = async (path: string, body: object) => {
-    if (typeof value === 'string' || value == null) return;
-    const getHeader = () => ({ Authorization: 'Bearer ' + value.access_token });
-    let resp = await axios.put(BACKEND_URL + path, body, {
-      headers: getHeader(),
-    });
-    if (resp.status >= 400) {
-      await ensureTokenValidity();
-      resp = await axios.patch(BACKEND_URL + path, body, {
+    const headers = getHeader();
+    if (!headers) return null;
+    return withRetry(() =>
+      axios.put(BACKEND_URL + path, body, {
         headers: getHeader(),
-      });
-    }
-    return resp;
+      }),
+    );
+  };
+
+  const deleteApi = async (path: string) => {
+    const headers = getHeader();
+    if (!headers) return null;
+    return withRetry(() =>
+      axios.delete(BACKEND_URL + path, { headers: getHeader() }),
+    );
   };
 
   return {
@@ -78,6 +93,7 @@ export const useOurApi: () => ApiHook = () => {
     postApi,
     patchApi,
     putApi,
+    deleteApi,
   };
 };
 
@@ -89,68 +105,64 @@ export const useOurApi: () => ApiHook = () => {
  */
 export const useSpotifyApi: () => ApiHook = () => {
   const { value, ensureTokenValidity } = useAuth();
-  const getApi = async (path: string) => {
-    if (typeof value === 'string' || value == null) return;
-    const getHeader = () => ({
-      Authorization: 'Bearer ' + value.spotify_access_token,
-    });
-    let resp = await axios.get(path, { headers: getHeader() });
-    if (resp.status >= 400) {
-      await ensureTokenValidity();
-      resp = await axios.get(path, { headers: getHeader() });
+
+  async function withRetry(
+    toCall: () => Promise<AxiosResponse<any>>,
+  ): Promise<AxiosResponse<any>> {
+    try {
+      const resp = await toCall();
+      return resp;
+    } catch (error) {
+      try {
+        await ensureTokenValidity();
+        const secondAttempt = await toCall();
+        return secondAttempt;
+      } catch (otherError) {
+        console.log(otherError);
+        throw otherError;
+      }
     }
-    return resp;
+  }
+
+  const getHeader = () => {
+    if (typeof value === 'string' || value == null) return null;
+    return { Authorization: 'Bearer ' + value.access_token };
+  };
+
+  const getApi = async (path: string) => {
+    const headers = getHeader();
+    if (!headers) return null;
+    return withRetry(() => axios.get(path, { headers: getHeader() }));
   };
 
   const postApi = async (path: string, body: object) => {
-    if (typeof value === 'string' || value == null) return;
-    const getHeader = () => ({
-      Authorization: 'Bearer ' + value.spotify_access_token,
-    });
-    let resp = await axios.post(path, body, {
-      headers: getHeader(),
-    });
-    if (resp.status >= 400) {
-      await ensureTokenValidity();
-      resp = await axios.post(path, body, {
+    const headers = getHeader();
+    if (!headers) return null;
+    return withRetry(() =>
+      axios.post(path, body, {
         headers: getHeader(),
-      });
-    }
-    return resp;
+      }),
+    );
   };
 
   const patchApi = async (path: string, body: object) => {
-    if (typeof value === 'string' || value == null) return;
-    const getHeader = () => ({
-      Authorization: 'Bearer ' + value.spotify_access_token,
-    });
-    let resp = await axios.patch(path, body, {
-      headers: getHeader(),
-    });
-    if (resp.status >= 400) {
-      await ensureTokenValidity();
-      resp = await axios.patch(path, body, {
+    const headers = getHeader();
+    if (!headers) return null;
+    return withRetry(() =>
+      axios.patch(path, body, {
         headers: getHeader(),
-      });
-    }
-    return resp;
+      }),
+    );
   };
 
   const putApi = async (path: string, body: object) => {
-    if (typeof value === 'string' || value == null) return;
-    const getHeader = () => ({
-      Authorization: 'Bearer ' + value.spotify_access_token,
-    });
-    let resp = await axios.put(path, body, {
-      headers: getHeader(),
-    });
-    if (resp.status >= 400) {
-      await ensureTokenValidity();
-      resp = await axios.patch(path, body, {
+    const headers = getHeader();
+    if (!headers) return null;
+    return withRetry(() =>
+      axios.put(path, body, {
         headers: getHeader(),
-      });
-    }
-    return resp;
+      }),
+    );
   };
 
   return {
